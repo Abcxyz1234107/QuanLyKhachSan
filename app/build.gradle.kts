2a259b8c2f1daa512be7088ea605c532cc8e05a1
Abcxyz1234107 <minecraftzcc6@gmail.com>
2025-06-12 21:54:57 +0700

    Thêm material design, và các theme, color
    Note: Lỗi không nhận colorBackground

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ android {
 
 dependencies {
 
+    implementation("com.google.android.material:material:1.12.0")
+
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.livedata.ktx)
 
