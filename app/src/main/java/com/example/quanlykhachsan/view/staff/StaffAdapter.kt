17d1123507e5ae7ce0b1cd378379c85eba120eaf
Abcxyz1234107 <minecraftzcc6@gmail.com>
2025-06-17 13:20:55 +0700

    Tạo file cho UI nhân viên (chưa có code)

---
+++ app/src/main/java/com/example/quanlykhachsan/view/staff/StaffAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.quanlykhachsan.view.staff
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.quanlykhachsan.data.local.entity.NhanVien
+
+class StaffAdapter {
+}
---
+++ app/src/main/java/com/example/quanlykhachsan/view/staff/StaffFragment.kt
@@ -0,0 +1,8 @@
+package com.example.quanlykhachsan.view.staff
+
+import androidx.fragment.app.Fragment
+import com.example.quanlykhachsan.R
+
+class StaffFragment : Fragment(R.layout.fragment_room_type) {
+
+}
